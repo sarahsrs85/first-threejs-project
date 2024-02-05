cad79b22d767ac9015993f8f29a48b2190feb959
Sarah Simpson <simpson@dayshiftdigital.com>
2024-02-05 12:38:17 -0500

    cube endered

--- script.js
+++ script.js
@@ -1,5 +1,6 @@
 import * as THREE from 'three'
 
+const canvas = document.querySelector('canvas.webgl');
 //scene
 const scene = new THREE.Scene();
 
@@ -9,3 +10,22 @@ const material = new THREE.MeshBasicMaterial({ color : 0xff000 })
 const mesh = new THREE.Mesh(geometry, material);
 
 scene.add(mesh);
+
+//sizes
+const sizes = {
+    width:800,
+    height:600
+}
+//camera
+const camera = new THREE.PerspectiveCamera(75, sizes.width / sizes.height);
+camera.position.z = 3;
+
+scene.add(camera);
+
+//renderer
+const renderer = new THREE.WebGLRenderer({
+    canvas : canvas
+});
+
+renderer.setSize(sizes.width, sizes.height);
+renderer.render(scene, camera);
